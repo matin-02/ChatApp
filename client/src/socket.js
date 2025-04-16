c3c0ac8cbf14d972950ad9aa4fbe0da60a26b70b
matin-02 <mohammadmatinali786@gmail.com>
2025-04-16 11:37:03 +0530

    ChatApp

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io("https://your-backend-name.onrender.com");
+const socket = io("https://chatapp-7t52.onrender.com");
+
 
 export default socket;
